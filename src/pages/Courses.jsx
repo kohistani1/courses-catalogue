269b30b9737d347a06f1e7a2d6774383c92530d6
usman <kohistani5525@gmail.com>
2022-08-14 15:49:01 +0500

    refactored

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Course from '../components/Course';
 import '../courses.css';
 import courses from '../data';
 
 function Courses() {
+  const [row1] = useState(courses[0]);
+  const [row2] = useState(courses[1]);
+  const [row3] = useState(courses[2]);
+
   return (
     <div>
       <div className='row'>
-        {courses[0].map((course, idx) => (
+        {row1.map((course, idx) => (
           <Course key={idx} {...course} />
         ))}
       </div>
       <div className='row'>
-        {courses[1].map((course, idx) => (
+        {row2.map((course, idx) => (
           <Course key={idx} {...course} />
         ))}
       </div>
       <div className='row'>
-        {courses[2].map((course, idx) => (
+        {row3.map((course, idx) => (
           <Course key={idx} {...course} />
         ))}
       </div>
